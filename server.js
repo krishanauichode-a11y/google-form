41b0cfc507093ae545cb51921e55151989cb2e81
krishanauichode-a11y <krishanauichode@gmail.com>
2026-04-30 15:07:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -165,7 +165,7 @@ app.post("/share-interakt", async (req, res) => {
     type: "Template",
     template: {
       name: "entry_pass",
-      languageCode: "en_US", // ✅ FIXED
+      languageCode: "en", // ✅ FIXED
       bodyValues: [
         String(user.full_name || "User"),
         "Scan QR or Barcode at entry"
