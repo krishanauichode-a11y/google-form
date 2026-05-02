255f37aa2c626a7119bdd359a6916fdffda13747
krishanauichode-a11y <krishanauichode@gmail.com>
2026-05-02 13:49:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -159,10 +159,15 @@ app.post("/send-email", async (req, res) => {
 // 📧 EMAIL SETUP (ADD HERE)
 // ==============================
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // TLS
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
   }
 });
 
