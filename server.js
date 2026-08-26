d4da9d07240a0e9969716a86237d080ae8b9570c
krishanauichode-a11y <krishanauichode@gmail.com>
2026-08-26 17:32:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -970,13 +970,12 @@ app.get("/join", async (req, res) => {
         <div class="msg-box" id="msgBox"></div>
         <div class="helper">
           <p>📋 Your Entry Pass ID is the 7-character code<br>below the barcode on your pass image</p>
-          <p style="margin-top:8px;">Don't have your ID? <a href="tel:+919876543210">📞 Call Support</a></p>
+          <p style="margin-top:8px;">Don't have your ID? <a href="tel:+919156709542">📞 Call Support</a></p>
         </div>
       </div>
     </div>
     <div class="footer">
       <p>www.tusharbhumkar.com</p>
-      <p style="margin-top:4px;"><a href="tel:+919876543210" style="color:#448aff;text-decoration:none;">📞 Support: +91 98765 43210</a></p>
     </div>
   </div>
   <script>
